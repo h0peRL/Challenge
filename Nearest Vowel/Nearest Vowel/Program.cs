246347e9bc0f8d473cee5147740c52232e05f2ed
n¿co <nico.delvecchio@gmx.ch>
2023-09-13 11:26:05 +0200

    add complete challenge

---
+++ Nearest Vowel/Nearest Vowel/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Nearest_Vowel;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Eingabe Bitte");
+        string input = Console.ReadLine();
+        int[] output = Vowelsearcher.NearestVowel(input);
+
+        for (int i = 0; i < output.Length; i++)
+        {
+            Console.Write(output[i] + ", ");
+            
+        }
+
+    }
+}
---
+++ Nearest Vowel/Nearest Vowel/Vowelsearcher.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nearest_Vowel
+{
+    public class Vowelsearcher
+    {
+        private static readonly char[] VOWELS = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+
+        public static int[] NearestVowel(string str)
+        {
+            int[] result = new int[str.Length];
+
+            for (int i = 0; i < str.Length; i++) 
+            {
+                char c = str[i]; 
+
+                if (c == ' ')
+                {
+                    result[i] = 1;
+                }
+                else if (VOWELS.Contains(c))
+                {
+                    result[i] = 0;
+                }
+                else 
+                {
+                    int distance = 0;
+                    //bool condition = true;
+                    while (true)
+                    {
+                        if (!VOWELS.Contains(c))
+                        {
+                            distance++;
+                        }
+                        //condition = false;
+                        break;
+                    }
+                    result[i] = distance;
+                }
+            }
+
+            return result;
+        }
+
+    }
+}
